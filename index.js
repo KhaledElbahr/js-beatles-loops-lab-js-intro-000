cc10f87ce690c6ff04cf23b9135e11e0a09836db
Khaled Elbahr <khaledelbahr2016@gmail.com>
2018-12-01 19:12:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+function theBeatlesPlay (musicians , instruments) {
+  var allMusicians = [];
+  for(let i = 0; i < musicians.length; i++) {
+        allMusicians.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return allMusicians;
+}
+
+function johnLennonFacts () {
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+}
